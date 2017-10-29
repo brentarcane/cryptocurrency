1cbd0608489d052cd2dafeb2262c21b843a6aa58
Brent Kozjak <brent.kozjak@breachaware.com>
2017-10-29 20:21:59 +0000

    Add dual mining batch file for Ethereum and Decred

---
+++ ethereum/start_dual_miner.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+EthDcrMiner64.exe -epool eu1.ethermine.org:4444 -ewal 0xcac8bf600be09466ede92ca946774580166ad374.sindel -epsw x -dpool stratum+tcp://dcr.suprnova.cc:3252 -dwal DsZ7cgrNoXdXhB79UL2gHfdpa1d5fiVk6wZ -dpsw x
